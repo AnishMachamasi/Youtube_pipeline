c0b052f77f0265e6678fb7cc335396acd592c5cc
developer-prashant-999 <mdrrrrprashant@gmail.com>
2023-02-03 12:19:24 +0545

    frame extraction updated

--- from_anysite.py
+++ from_anysite.py
@@ -74,47 +74,45 @@ def download_audio(url, audio_format):
 
 
 def extract_frames(video_name):
-    # Extract the frames using OpenCV
-    # Open the video using OpenCV
-    video = cv2.VideoCapture("video/"+video_name)
-
-    # Get the video frame count
-    frame_count = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
-    print(frame_count)
-
-    # # Get the video name
-    # video_name = url.split("/")[-1]
-
-    # Create a directory to store the frames
-    os.makedirs("frames/"+video_name, exist_ok=True)
+    vid = cv2.VideoCapture("F:/coderush_assignment/Youtube_pipeline/video/"+video_name)
     
-    # Ask the user whether they want to resize the frames
-    resize_frames = input("Do you want to resize the frames? (yes/no): ")
-    frame_width = 0
-    frame_height = 0
-    
-    # If the user wants to resize the frames, ask for the desired size
-    if resize_frames == "yes":
-        frame_width = int(input("Enter the desired width of the frames: "))
-        frame_height = int(input("Enter the desired height of the frames: "))
-    
-    # Extract the frames
-    for i in range(frame_count):
-        success, frame = video.read()
+    try:
+
+        # creating a folder named data
+        if not os.path.exists('data'):
+            os.makedirs('data')
+
+    # if not created then raise error
+    except OSError:
+        print('Error: Creating directory of data')  
+    currentframe = 0
+    count=0
+    while (True):
+
+        # reading from frame
+        success, frame = vid.read()
+
         if success:
-            # Resize the frame if necessary
-            if resize_frames == "yes":
-                frame = cv2.resize(frame, (frame_width, frame_height))
-            cv2.imwrite("frames/"+video_name+"/frame"+str(i)+".jpg", frame)
-        if i % 50 == 0:
-            print(f"Processed {i} frames.")
-    print("Frames extracted.")
-    
-    # Release the video
-    video.release()
-    
+            if count%20==0:
+                # continue creating images until video remains
+                name = './data/frame' + str(currentframe) + '.jpg'
+                print('Creating...' + name)
+
+                # writing the extracted images
+                cv2.imwrite(name, frame)
+
+                # increasing counter so that it will
+                # show how many frames are created
+                currentframe += 1
+            count+=1
+        else:
+            break
+    vid.release()
+    cv2.destroyAllWindows()
+
 
 video_name = download_video(url, video_format)
+
 def monitor_pipeline(url, video_name):
     video_file = url.split("/")[-1] + ".mp4"
     if not os.path.exists("video/" + video_name):
